f1be1069fd669513cbbf971f185f650dc4218db1
ZUNbado <git@acs.li>
2013-12-28 00:09:40 +0100

    fixed log_rcvd

--- bin/policies/END-OF-MESSAGE/log_rcvd.sh
+++ bin/policies/END-OF-MESSAGE/log_rcvd.sh
@@ -1,7 +1,6 @@
-SQL="SELECT 'EXIST' AS QUERYTEST FROM log_rcvd WHERE sender='${sender}' AND rcpt='${recipient}'"
-QUERYTEST=$(${MYSQL} "${SQL}"|grep -v QUERYTEST)
+QUERYTEST=$(pbsql "SELECT 'EXIST' AS QUERYTEST FROM log_rcvd WHERE sender='${sender}' AND rcpt='${recipient}'")
 if [ $(echo ${QUERYTEST} |grep "EXIST"|wc -l) -gt 0 ]; then
-	UPDATE=$(${MYSQL} "UPDATE log_rcvd SET count=count+1, updated=NOW() WHERE sender='$sender' AND rcpt='$recipient'")
+	UPDATE=$(pbsql "UPDATE log_rcvd SET count=count+1, updated=NOW() WHERE sender='$sender' AND rcpt='$recipient'")
 else
-	INSERT=$(${MYSQL} "INSERT INTO log_rcvd (sender, rcpt, count, created) VALUES ('$sender', '$recipient', 1, NOW()))")
+	INSERT=$(pbsql "INSERT INTO log_rcvd (sender, rcpt, count, created) VALUES ('$sender', '$recipient', 1, NOW())")
 fi
